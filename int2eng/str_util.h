c098e4740216c0410a474bc54df3f70e91857079
fontesrp <fontesr2@gmail.com>
2016-12-17 16:38:00 -0200

    Add header file with useful string functions

---
+++ int2eng/str_util.h
@@ -0,0 +1,13 @@
+//
+//  str_util.h
+//  int2eng
+//
+//  Created by Rodrigo Fontes on 17/12/16.
+//  Copyright © 2016 Rodrigo Fontes. All rights reserved.
+//
+
+#ifndef str_util_h
+#define str_util_h
+
+
+#endif /* str_util_h */
